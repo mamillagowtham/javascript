c3ee6136721363b8f235870fc469a6290c6af97d
mamillagowtham <mamillagowtham@gmail.com>
2024-08-28 15:44:53 +0530

    constructor

---
+++ javascript-basics/constructor/constructor.js
@@ -0,0 +1,31 @@
+//this === window  
+// this keyword is refers to a global object.(window object)
+// this keyword is a special keyword it is referred as object.
+
+
+// contructor
+// when ever you written "this" keyword inside the function it  will be act as constructor and add "new" to the function then only it will recognize 
+// that, it was a constructor.
+
+
+
+ /*assigning a key and value to the object with the help of constructor
+   you can assign with three methods
+   1.call 2.apply 3.bind
+
+1. by using call method u can assign key&value pair to the object  at function call just add like this .call beside arguments send the object name 
+   which object you want to update
+
+
+2. by using call method u can assign key&value pair to the object  at function call just add like this .apply beside arguments send the object name 
+   which object you want to update,  arguments must in array format
+
+
+3. by using call method u can assign key&value pair to the object  at function call just add like this .apply beside arguments send the object name 
+   which object you want to update,   call the function seperatly.
+
+
+   */
+
+
+
